8b73c6fc36ba2daab987cafffc6e0345bc32ddf5
Nicola Cannata <nicola@create-react-app.it>
2019-11-28 16:37:02 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
 
 const App: React.FC = () => {
+  const [data, setData] = useState([]);
+
+  const [query, setQuery] = useState("posts");
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/${query}?_start=0&_limit=10`
+    );
+    setData(await response.json());
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query]);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="posts" onClick={() => setQuery("posts")}>
+        POSTS
+      </button>
+      <button onClick={() => setQuery("albums")}>ALBUMS</button>
+      <button onClick={() => setQuery("photos")}>PHOTOS</button>
+      <button onClick={() => setQuery("todos")}>TODOS</button>
+      <hr />
+      {data.map(data => (
+        //@ts-ignore
+        <p>{data.title}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
